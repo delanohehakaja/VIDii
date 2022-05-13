cd844f0515474bbaef4ce32a30b3185c66513eff
delanohehakaja <d.d.hehakaja@gmail.com>
2022-05-13 10:45:03 +0200

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,24 @@
 // JavaScript Document
-console.log("Howdy!");
+var maguireButton = document.querySelector("#maguire");
+var garfieldButton = document.querySelector("#garfield");
+var hollandButton = document.querySelector("#holland");
+
+console.log(maguireButton);
+console.log(garfieldButton);
+console.log(hollandButton);
+
+maguireButton.addEventListener("click", openMaguireVerse);
+garfieldButton.addEventListener("click", openGarfieldVerse);
+hollandButton.addEventListener("click", openHollandVerse);
+
+function openMaguireVerse() {
+    document.body.dataset.verse = "maguire";
+}
+
+function openGarfieldVerse() {
+    document.body.dataset.verse = "garfield";
+}
+
+function openHollandVerse() {
+    document.body.dataset.verse = "holland";
+}
